be9e26d2a38b439b9a07688785c085fe892fcb82
Todd Stout <todd.tstout@gmail.com>
2021-08-09 18:25:57 -0500

    init plugin test

--- src/plugin_b/core.clj
+++ src/plugin_b/core.clj
@@ -1,4 +1,7 @@
 (ns plugin-b.core
   (:require [clojure.tools.cli :refer [parse-opts]]))
 
+(defn init []
+  (println "--->>> plugin-b loaded <<<------"))
+
 (defn -main [& args] (println "hello world"))
